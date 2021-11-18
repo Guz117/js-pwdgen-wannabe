dde5d7f02d5b4a8cf6eb1aed05234657ded6ec26
Guz117 <cristian.guzman117@gmail.com>
2021-11-18 14:52:11 +0100

    visualizzazione risultati

--- js/script.js
+++ js/script.js
@@ -13,5 +13,9 @@ console.log('Cognome utente', cognomeUtente);
 let coloreUtente = prompt('Inserisci il tuo colore preferito');
 console.log('Colore preferito utente', coloreUtente); 
 
-let infoUtente = ` ${nomeUtente} ${cognomeUtente} ${coloreUtente} 21 `
+let infoUtente = `Nome: ${nomeUtente}<br>
+Cognome: ${cognomeUtente}<br>
+Colore preferito: ${coloreUtente} 21`
 console.log(infoUtente)
+
+document.getElementById('mio_id').innerHTML = infoUtente
