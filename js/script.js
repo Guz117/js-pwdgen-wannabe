4d2f95a0740e78e58b170a45b2b679e919e5f814
Guz117 <cristian.guzman117@gmail.com>
2021-11-18 14:39:15 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Descrizione esercizio:
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+let nomeUtente = prompt('Inserisci il tuo nome');
+console.log('Nome utente',  nomeUtente); 
+
+let cognomeUtente = prompt('Inserisci il tuo cognome');
+console.log('Cognome utente', cognomeUtente); 
+
+let coloreUtente = prompt('Inserisci il tuo colore preferito');
+console.log('Colore preferito utente', coloreUtente); 
+
+let infoUtente = ` ${nomeUtente} ${cognomeUtente} ${coloreUtente} 21 `
+console.log(infoUtente)
